440a24a3c0e2c99433249ebfbf6233428aa2f3ca
Silvano Xavier <sxconect@gmail.com>
2025-11-20 14:25:28 -0300

    [dyad] wrote 3 file(s), deleted 9 file(s), added prisma, @prisma/client package(s)

--- convex/schemas/departments.ts
+++
@@ -1,8 +0,0 @@
-import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
-
-export const departmentsSchema = defineTable({
-  companyId: v.id("companies"),
-  name: v.string(),
-  createdAt: v.number(),
-}).index("by_company_id", ["companyId"]);

713cb9897a453e51568807cf309275c559b3b5e6
Silvano Xavier <sxconect@gmail.com>
2025-11-23 10:42:06 -0300

    [dyad] Fix TS2339: Tipos ImportMetaEnv no vite-env.d.ts - wrote 1 file(s)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+  readonly VITE_EVOLUTION_API_URL: string;
+  readonly VITE_EVOLUTION_API_KEY: string;
+  readonly VITE_SENTRY_DSN: string;
+  // more env variables can be defined by this type
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
